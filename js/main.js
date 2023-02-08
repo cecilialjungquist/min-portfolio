e2881c353db508581bdb78d4c6e9e45617762765
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-02-08 16:20:23 +0100

    Started working on apis

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+const apiContainer = document.getElementById('api-container');
+const BASE_URL = 'http://numbersapi.com/random/trivia';
 
+async function fetchFact() {
+    try {
+        const response = await fetch(BASE_URL);
+        console.log(response.url);
+        let fact = await fetch(response.url);
+        fact = await fact.json();
+        console.log(fact);
+        const apiEl = document.createElement('p');
+        
+    } catch (error) {
+        
+    }
+}
 
+fetchFact();
