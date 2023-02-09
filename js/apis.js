53eb3110cfa4ee66da459d945f59517a0d6ad464
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-02-09 10:17:48 +0100

    input done

--- js/apis.js
+++ js/apis.js
@@ -1,16 +1,46 @@
 const apiContainer = document.getElementById('api-container');
-const BASE_URL = 'http://numbersapi.com/random/trivia?json';
+const notFoundMsgEl = document.createElement('p');
+const BASE_URL = 'http://numbersapi.com/';
+const END_URL = '/trivia?json'
 
 async function fetchFact() {
+
+    notFoundMsgEl.innerHTML = '';
+    let num = fetchInput();
+
     try {
-        const response = await fetch(BASE_URL);
+        let response = await fetch(BASE_URL + num + END_URL);
         let funFact = await response.json();
+
+        if (!funFact.found) {
+            response = await fetch(BASE_URL + 'random' + END_URL);
+            funFact = await response.json();
+            notFoundMsgEl.classList.add('help-text');
+            notFoundMsgEl.innerHTML = `${num} seems like a boring number. but we've found this fun fact instead:`;
+            apiContainer.insertAdjacentElement('beforebegin', notFoundMsgEl);
+        } 
+            
         funFact = funFact.text.toLowerCase();
         apiContainer.innerHTML = funFact;
+
     } catch (error) {
         console.log('Oops! There is an error: ' + error);
-        apiContainer.innerHTML = 'ooops, something went wrong. please try again!'
+        apiContainer.innerHTML = 'ooops, something went wrong. please try again!';
     }
 }
+
+function fetchInput() {
+    let numInput = document.getElementById('num-input');
+    numInput = numInput.value;
+
+    if (numInput === '') {
+        numInput = Math.floor(Math.random() * 100)
+    } else if (numInput < 0 ) {
+        numInput = numInput * (-1);
+    }
+
+    return numInput;
+}
+
 
 export default fetchFact;
